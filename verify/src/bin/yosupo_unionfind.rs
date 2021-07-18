ceeca79f262db913767e68899f163e56601210f8
taotao54321 <taotao54321@gmail.com>
2021-07-19 01:59:04 +0900

    verify: yosupo unionfind

---
+++ verify/src/bin/yosupo_unionfind.rs
@@ -0,0 +1,31 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+
+use proconio::input;
+
+use procon_rs_union_find::*;
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+    }
+
+    let mut uf = UnionFind::new(n);
+
+    for _ in 0..q {
+        input! { cmd: u32 }
+
+        match cmd {
+            0 => {
+                input! { v: usize, w: usize }
+                uf.unite(v, w);
+            }
+            1 => {
+                input! { v: usize, w: usize }
+                let ans = if uf.is_same(v, w) { 1 } else { 0 };
+                println!("{}", ans);
+            }
+            _ => panic!("invalid command: {}", cmd),
+        }
+    }
+}
